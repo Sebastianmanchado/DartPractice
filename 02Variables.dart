f4b62f9dcb07945460acf8440a3ac8443f3b3cc7
sebastianmanchado <sebastianmanchado.laboral@gmail.com>
2026-02-25 14:04:21 -0300

    variables

---
+++ 02Variables.dart
@@ -0,0 +1,18 @@
+//Variables
+
+void main(){
+  var nombre = 'Sebas';
+  var edad = 25; 
+
+  //Obtengo el tipo de dato
+
+  var tipoDeDatoNombre = nombre.runtimeType ;
+  var tipoDeDatoEdad = edad.runtimeType ;
+
+  print('$nombre tiene $edad años');
+  print('El tipo de dato de nombre es $tipoDeDatoNombre');
+  print('El tipo de dato de edad es $tipoDeDatoEdad');
+
+  
+
+}
