1a7777e7815e8482b91c3329ec16ba23fa642918
sebastianmanchado <sebastianmanchado.laboral@gmail.com>
2026-02-28 19:38:44 -0300

    set

---
+++ 10Set.dart
@@ -0,0 +1,10 @@
+void main(){
+  //set = lista de valores unicos
+  var miPrimerSet = <String>['Luis','Lucia','Juan'];
+  miPrimerSet.add('Seba');
+  miPrimerSet.add('Luis'); // No añade na 
+
+  miPrimerSet.clear();
+  print(miPrimerSet); 
+
+}
