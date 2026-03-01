50bed0df3e9bd08ed7389856a9be3b837bd32ff9
sebastianmanchado <sebastianmanchado.laboral@gmail.com>
2026-03-01 20:00:21 -0300

    operadores de asignación

---
+++ 18OperadoresAsignacion.dart
@@ -0,0 +1,28 @@
+void main(){
+  int a = 10;
+  int b = 25; 
+
+  var c;
+
+  c ??= 1; //Si c es nula, asigna valor 1 
+
+
+  c+= a; // asigno a c el valor de c+a 
+  print(c);
+  c -= a; //asigno a c el valor de c-a 
+  print(c);
+  c*=a;
+  print(c);
+  c/=a;
+  print(c);
+  c~/=a;
+  print(c);
+  c%=a;
+  print(c);
+  
+
+
+
+
+
+}
