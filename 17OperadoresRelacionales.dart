7fbae8a346e980433e1011979be4d7f96cd8e2d1
sebastianmanchado <sebastianmanchado.laboral@gmail.com>
2026-03-01 19:44:40 -0300

    Operadores relacionales

---
+++ 17OperadoresRelacionales.dart
@@ -0,0 +1,34 @@
+void main(){
+
+  // 3) OPERADORES DE COMPARACIÓN
+// ==  igual
+// !=  distinto
+// >   mayor que
+// <   menor que
+// >=  mayor o igual
+// <=  menor o igual
+// Devuelven true o false
+
+
+
+  int a = 10; 
+  int b = 25; 
+
+  var mayor = a > b; 
+  var menor = a < b ;
+  var mayorIgual = a >= b;
+  var menorIgual = a <= b; 
+  var igual = a == b; 
+  var distinto = a!=b; 
+
+  //V o F 
+  print(mayor);
+  print(mayorIgual);
+  print(menor);
+  print(mayorIgual);
+  print(igual);
+  print(distinto);
+
+
+
+}
