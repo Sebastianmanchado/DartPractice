a8ef140fce47a8d1fd2e3059222d536c49da588a
sebastianmanchado <sebastianmanchado.laboral@gmail.com>
2026-03-01 19:39:17 -0300

    operadores aritmeticos

---
+++ 16OperadoresAritmeticos.dart
@@ -0,0 +1,33 @@
+void main(){
+  // Operadores aritmeticos
+  int a = 10; 
+  int b = 25; 
+
+  //suma
+  print(a+b);
+
+  //resta 
+  print(b -a); 
+
+  //multiplicacion
+  print(a*b);
+
+  //division
+  print(a/b); //convierte 2 enteros en un double
+
+  //division entera
+  print(b~/a);
+
+  //resto
+  print(b%a); 
+
+
+   //incremento/ decremento 
+   print(a);
+   a++;
+   print(a);
+   a--;
+   print(a);
+
+   
+}
