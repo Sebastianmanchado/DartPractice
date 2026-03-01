0fcaba1523939141d13b1cbf54028a42f4c862ae
sebastianmanchado <sebastianmanchado.laboral@gmail.com>
2026-03-01 20:11:45 -0300

    Ooperadores Logicos

---
+++ 19OperadoresLogicos.dart
@@ -0,0 +1,16 @@
+void main(){
+  bool soyBuenProfe = false;
+  print(soyBuenProfe);
+  print(!soyBuenProfe); // Imprime valor contrario
+
+  var a = 10; 
+  var b = 25; 
+
+  bool esVerdad = a>20 || b > 20; // and
+  print(esVerdad);
+
+  bool esVerdad2 = a>20 && b>50; // or
+  print(esVerdad2);
+
+  
+}
